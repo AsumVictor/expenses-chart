a651db2b65b44feb33c50341d377ad9345b1a0fb
AsumVictor <iamasum369@gmail.com>
2022-09-28 19:27:47 +0000

    Set up

---
+++ config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      
+      extend: {
+        colors: {
+          DarkBrown: 'hsl(25, 47%, 15%)',
+          MediumBrown: 'hsl(28, 10%, 53%)',
+          Cream: 'hsl(27, 66%, 92%)',
+          VeryPaleOrange: 'hsl(33, 100%, 98%)',
+          SoftRed: 'hsl(10, 79%, 65%)',
+          Cyan: 'hsl(186, 34%, 60%)'
+        }
+      }
+    }
+  }
