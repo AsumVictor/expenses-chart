822aaa505909adac63d67430a9121d24a9765eb7
AsumVictor <iamasum369@gmail.com>
2022-09-28 22:04:29 +0000

    added height

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener('load',()=>{
-    graph=[
+    graphBars=[
         {
           day: "mon",
           amount: 17.45
@@ -30,14 +30,19 @@ window.addEventListener('load',()=>{
         }
       ]
 
-    const graphContent = `
-    <div class="tooltip p-1 rounded-md font-bold">$17.45</div>
-    <div class="bar bg-SoftRed rounded-md mt-1" style="height:3cm;width:1cm;"></div>
-    <p class="x-axis-days font-bold text-gray-500">Mon</p>
-    `
    const graphArea = document.querySelector('.graph')  
-   const bar = document.createElement('div') 
-    bar.setAttribute('class','barsContainer cursor-pointer flex flex-col items-center')
-    bar.innerHTML = graphContent;
-    graphArea.appendChild(bar)
+      graphBars.forEach(graphBar=> {
+        let calHeight = ((graphBar.amount*3)/52.36)
+        let height = calHeight.toFixed(2) + "cm"
+        const graphContent = `
+        <div class="tooltip p-1 rounded-md font-bold">$${graphBar.amount}</div>
+        <div class="bar bg-SoftRed rounded-md mt-1" style="--height:${height};"></div>
+        <p class="x-axis-days font-bold text-gray-500">${graphBar.day}</p>
+        `
+        let bar = document.createElement('div') 
+        bar.setAttribute('class','barsContainer cursor-pointer flex flex-col items-center')
+        bar.innerHTML = graphContent;
+        graphArea.appendChild(bar)
+      }); 
+   
 })
