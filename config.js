16f740d74b111adb3c04fe01f259c7992c5627bf
AsumVictor <iamasum369@gmail.com>
2022-09-28 20:48:50 +0000

    chart footer

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ tailwind.config = {
           DarkBrown: 'hsl(25, 47%, 15%)',
           MediumBrown: 'hsl(28, 10%, 53%)',
           Cream: 'hsl(27, 66%, 92%)',
-          VeryPaleOrange: 'hsl(33, 100%, 98%)',
+          VeryPaleOrange: 'hsl(30, 58%, 91%)',
+          VeryPaleOrange1: 'hsl(33, 100%, 98%)',
           SoftRed: 'hsl(10, 79%, 65%)',
           Cyan: 'hsl(186, 34%, 60%)'
         }
